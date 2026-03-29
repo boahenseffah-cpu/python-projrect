9aa416df2e18d5acc6c0fc51fa8c4ab363874511
boahenseffah-cpu <boahen.seffah@gmail.com>
2026-03-29 17:56:53 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -3,12 +3,21 @@ WIDTH=811
 HEIGHT=450
 boy=Actor("child")
 boy.pos=(700,350)
+mom=Actor("mom")
+mom.pos=(620,350)
 def draw():
     screen.blit("background",(0,0))
     boy.draw()
-
-
-
-
+    mom.draw()
+def update():
+    if keyboard.up:
+        mom.y-=5
+    elif keyboard.down:
+        mom.y+=5
+    elif keyboard.left:
+        mom.x-=5
+    elif keyboard.right:
+        mom.x+=5
 pgzrun.go()
+
 
