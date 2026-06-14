a8e66dd0be13a08f81ad4299e2795106bed61d20
boahenseffah-cpu <boahen.seffah@gmail.com>
2026-06-14 17:54:56 +0200

    Add files via upload

--- obstaclegame.py
+++ obstaclegame.py
@@ -1,5 +1,7 @@
 import pgzrun
 import random
+gameover=False
+life=3
 HEIGHT=522
 WIDTH=696
 mario=Actor("mario")
@@ -8,18 +10,37 @@ obstacleimages=["bombe","cactus","rock","spike"]
 obstaclelist=[]
 def newobstacle():
     obstacle=Actor(random.choice(obstacleimages))
-    obstacle.pos=(670,470)
+    obstacle.pos=(670,400)
     obstaclelist.append(obstacle)
     clock.schedule(newobstacle,2)
 def draw():
+    if gameover==True:
+        screen.blit("gameover",(50,-20))
+        return
     screen.blit("mariobackground",(0,0))
+    screen.draw.text("life="+str(life),(10,10))
     mario.draw()
     for obstacle in obstaclelist:
         obstacle.draw()
+def on_key_down(key):
+    if key==keys.SPACE and mario.y>350:
+        mario.y-=180
 
 def update():
-    for obstacle in obstaclelist:
+    global life
+    global gameover
+    if mario.y<400:
+        mario.y+=2   
+    for obstacle in obstaclelist: 
         obstacle.x-=2
+        if mario.colliderect(obstacle):
+            life-=1
+
+            obstaclelist.remove(obstacle)
+
+        if life==0:
+            gameover=True
+
 
 
     
