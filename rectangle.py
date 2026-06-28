1055941cb35821b4a69f3aeab3e76bdab3bbe90b
boahenseffah-cpu <boahen.seffah@gmail.com>
2026-06-28 17:58:43 +0200

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,14 @@
+class Rectangle():
+    def __init__(self,length,width):
+        self.length=length
+        self.width=width
+    def area(self):
+        print(self.length*self.width)
+        
+    
+rectangle1=Rectangle(8,16)
+rectangle1.area()
+
+
+
+        
