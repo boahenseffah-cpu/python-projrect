4b05757520b987bc38bcfe767f02638df0f319a6
boahenseffah-cpu <boahen.seffah@gmail.com>
2026-07-12 17:08:10 +0200

    Add files via upload

--- rectangle.py
+++ rectangle.py
@@ -9,6 +9,18 @@ class Rectangle():
 rectangle1=Rectangle(8,16)
 rectangle1.area()
 
-
-
+class square():
+    def __init__(self,length,width):
+        self.length=length
+        self.width=width
+    def perimeter(self):
+        print((self.length+self.width)*2)
         
+    
+square1=square(8,16)
+square1.perimeter()
+
+
+
+
+
